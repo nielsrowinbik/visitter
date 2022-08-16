0dffe54dd240d6571019856b8e0197b6f4f8efcd
Niels Bik <nielsrowinbik@gmail.com>
2022-08-16 12:08:59 +0200

    Add support for custom layouts

--- apps/manage/pages/_app.tsx
+++ apps/manage/pages/_app.tsx
@@ -1,22 +1,36 @@
-import type { AppProps } from "next/app";
+import type { AppProps as NextAppProps } from "next/app";
+import type { NextComponentType } from "next";
 import Head from "next/head";
+import type { ReactNode } from "react";
 import { initAuth } from "../lib/auth";
 
 import "../styles/globals.css";
 
 initAuth();
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <title>Visitter</title>
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width"
-      />
-    </Head>
-    <Component {...pageProps} />
-  </>
-);
+type GetLayoutFunction = (page: ReactNode) => ReactNode;
+
+type AppProps = NextAppProps & {
+  Component: NextComponentType & {
+    getLayout: GetLayoutFunction;
+  };
+};
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <>
+      <Head>
+        <title>Visitter</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
+};
 
 export default App;
