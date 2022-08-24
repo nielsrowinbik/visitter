d3d730849fb3b5326a7f1d32cf4cbfe7f294c593
Niels Bik <nielsrowinbik@gmail.com>
2022-08-24 20:44:20 +0200

    Rework API to fetch and delete a specific home

--- apps/manage/pages/api/homes/[homeId]/index.ts
+++ apps/manage/pages/api/homes/[homeId]/index.ts
@@ -1,38 +1,57 @@
-import { deleteHome, findHomeById, updateHome } from "@lib/homes";
-
 import type { NextApiHandler } from "next/types";
+import { getErrorMessage } from "@lib/errors";
+import { getSession } from "@lib/auth/session";
+import nc from "next-connect";
+import prisma from "@db";
 
-const handler: NextApiHandler = async (req, res) => {
-  // // TODO: Move the block starting from here until the next comment into some kind of middleware
-  // // Check token presence:
-  // const token = req.headers?.authorization;
-  // if (!token) {
-  //   return res.status(403).json({ error: "Unauthorized" });
-  // }
-  // // Validate token
-  // const user = await verifyIdToken(token);
-  // // If user id is null, the token was invalid:
-  // if (user.id === null) {
-  //   return res.status(403).json({ error: "Unauthorized" });
-  // }
-  // // TODO: Move the code above from here until the previous comment into some kind of middleware
-  // const homeId = req.query.homeId as string;
-  // if (req.method === "GET") {
-  //   const data = await findHomeById(homeId);
-  //   return res.status(200).json(data);
-  // }
-  // if (req.method === "PATCH") {
-  //   const homeData = JSON.parse(req.body);
-  //   // TODO: Validate the home
-  //   const home = await updateHome(homeId, homeData);
-  //   return res.status(200).json(home);
-  // }
-  // if (req.method === "DELETE") {
-  //   await deleteHome(homeId);
-  //   return res.status(204).end();
-  // }
-  // // TODO: Which status code should we use to indicate that the method isn't supported?
-  // return res.status(400).end();
+const deleteHandler: NextApiHandler = async (req, res) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    await prisma.home.delete({
+      where: {
+        id: req.query.homeId as string,
+      },
+    });
+    return res.status(204).end();
+  } catch (error) {
+    console.error("[api] home", error);
+    return res
+      .status(500)
+      .json({ statusCode: 500, message: getErrorMessage(error) });
+  }
 };
 
-export default handler;
+const getHandler: NextApiHandler = async (req, res) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const home = await prisma.home.findFirst({
+      where: {
+        id: { equals: req.query.homeId as string },
+      },
+    });
+
+    if (home) return res.status(200).json(home);
+    return res.status(404).json({ message: "Not Found" });
+  } catch (error) {
+    console.error("[api] home", error);
+    return res
+      .status(500)
+      .json({ statusCode: 500, message: getErrorMessage(error) });
+  }
+};
+
+export default nc().delete(deleteHandler).get(getHandler);
