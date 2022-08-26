80f776f85aa722e0e7db0482193a3f76f920b8a1
Niels Bik <nielsrowinbik@gmail.com>
2022-08-26 21:59:40 +0200

    Remove back button

--- apps/manage/pages/[homeId]/index.tsx
+++ apps/manage/pages/[homeId]/index.tsx
@@ -1,13 +1,10 @@
 import type { Booking, Home } from "@prisma/client";
-import { Button, Space, Title } from "@mantine/core";
 
-import { ArrowLeftIcon } from "@heroicons/react/outline";
 import { BookingsList } from "@components/BookingsList";
 import { DashboardLayout } from "@components/Layouts/DashboardLayout";
 import type { GetServerSideProps } from "next/types";
 import { HomeDeleteButton } from "@components/HomeDeleteButton";
 import { HomeTitle } from "@components/HomeTitle";
-import Link from "next/link";
 import { SWRConfig } from "swr";
 import { getSession } from "@lib/auth/session";
 import prisma from "@db";
@@ -23,19 +20,9 @@ type PageProps = {
 const Page = ({ fallback, homeId }: PageProps) => (
   <SWRConfig value={{ fallback }}>
     <main>
-      <Link href="/dashboard" passHref>
-        <Button
-          compact
-          component="a"
-          variant="subtle"
-          leftIcon={<ArrowLeftIcon height={12} width={12} />}
-        >
-          Back to overview
-        </Button>
-      </Link>
       <HomeTitle homeId={homeId} />
       <BookingsList homeId={homeId} />
-      <h2 className="font-bold text-2xl">Danger zone</h2>
+      <h2 className="text-2xl font-bold">Danger zone</h2>
       <HomeDeleteButton homeId={homeId} />
     </main>
   </SWRConfig>
