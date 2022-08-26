d9e0549d57b3c7e40040c4a18922e45d48fd11f6
Niels Bik <nielsrowinbik@gmail.com>
2022-08-26 21:55:37 +0200

    Add Dark theme

--- apps/manage/pages/_document.tsx
+++ apps/manage/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 export default class _Document extends Document {
   render() {
     return (
-      <Html className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'ss01'] js-focus-visible">
+      <Html className="js-focus-visible h-full scroll-smooth bg-white text-zinc-800 antialiased [font-feature-settings:'ss01'] dark:bg-zinc-900 dark:text-white">
         <Head />
         <body className="flex h-full flex-col">
           <Main />
