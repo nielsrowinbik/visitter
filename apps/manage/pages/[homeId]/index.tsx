cbd5d8ac60542a28c6a39c0e300d36fd4f9eb41c
Niels Bik <nielsrowinbik@gmail.com>
2022-08-26 13:06:17 +0200

    Pre-render home detail page

--- apps/manage/pages/[homeId]/index.tsx
+++ apps/manage/pages/[homeId]/index.tsx
@@ -2,33 +2,22 @@ import { Button, Space, Title } from "@mantine/core";
 
 import { ArrowLeftIcon } from "@heroicons/react/outline";
 import { DashboardLayout } from "@components/Layouts/DashboardLayout";
-import { DeleteHomeButton } from "@components/DeleteHomeButton";
 import type { GetServerSideProps } from "next/types";
+import type { Home } from "@prisma/client";
+import { HomeDeleteButton } from "@components/HomeDeleteButton";
+import { HomeTitle } from "@components/HomeTitle";
 import Link from "next/link";
-import { fetcher } from "@lib/fetch";
+import { SWRConfig } from "swr";
 import { getSession } from "@lib/auth/session";
 import prisma from "@db";
-import { useRouter } from "next/router";
-import useSWR from "swr";
 
 type PageProps = {
-  // home: Home;
-  // past: Booking[];
-  // upcoming: Booking[];
+  fallback: Record<string, Home>;
+  homeId: string;
 };
 
-const Page = ({}: PageProps) => {
-  const { query } = useRouter();
-  const { homeId } = query;
-
-  const { data: home } = useSWR(
-    homeId ? `/api/homes/${homeId}` : null,
-    fetcher
-  );
-
-  if (!home) return <div>Loading...</div>;
-
-  return (
+const Page = ({ fallback, homeId }: PageProps) => (
+  <SWRConfig value={{ fallback }}>
     <main>
       <Link href="/dashboard" passHref>
         <Button
@@ -40,117 +29,17 @@ const Page = ({}: PageProps) => {
           Back to overview
         </Button>
       </Link>
-      <Title>{home.name}</Title>
+      <HomeTitle homeId={homeId} />
       <Space h="md" />
       <Title order={2}>Bookings</Title>
       <Space h="sm" />
-      {/* <Tabs defaultValue="upcoming">
-        <Tabs.List>
-          <Tabs.Tab value="upcoming">Upcoming</Tabs.Tab>
-          <Tabs.Tab value="past">Past bookings</Tabs.Tab>
-        </Tabs.List>
-
-        <Tabs.Panel value="upcoming" pt="xs">
-          <Stack>
-            {upcoming.map((booking: Booking) => (
-              <Box
-                sx={(theme) => ({
-                  backgroundColor:
-                    theme.colorScheme === "dark"
-                      ? theme.colors.dark[6]
-                      : theme.colors.gray[0],
-                  padding: theme.spacing.xl,
-                  borderRadius: theme.radius.md,
-                  height: "100%",
-                })}
-                key={booking.id}
-              >
-                <Text>
-                  From{" "}
-                  <strong>
-                    {format(fromUnixTime(booking.startDate / 1000), "d MMMM y")}
-                  </strong>{" "}
-                  until{" "}
-                  <strong>
-                    {format(fromUnixTime(booking.endDate / 1000), "d MMMM y")}
-                  </strong>
-                  .
-                </Text>
-                <DeleteBookingButton bookingId={booking.id} />
-              </Box>
-            ))}
-          </Stack>
-          <Space h="sm" />
-          <Link href={`/${homeId}/booking/new`} passHref>
-            <Button component="a" variant="light">
-              Add new booking
-            </Button>
-          </Link>
-        </Tabs.Panel>
-
-        <Tabs.Panel value="past" pt="xs">
-          <Stack>
-            {past.map((booking: Booking) => (
-              <Box
-                sx={(theme) => ({
-                  backgroundColor:
-                    theme.colorScheme === "dark"
-                      ? theme.colors.dark[6]
-                      : theme.colors.gray[0],
-                  padding: theme.spacing.xl,
-                  borderRadius: theme.radius.md,
-                  height: "100%",
-                })}
-                key={booking.id}
-              >
-                <Text>
-                  From{" "}
-                  <strong>
-                    {format(fromUnixTime(booking.startDate / 1000), "d MMMM y")}
-                  </strong>{" "}
-                  until{" "}
-                  <strong>
-                    {format(fromUnixTime(booking.endDate / 1000), "d MMMM y")}
-                  </strong>
-                  .
-                </Text>
-                <DeleteBookingButton bookingId={booking.id} />
-              </Box>
-            ))}
-          </Stack>
-        </Tabs.Panel>
-      </Tabs> */}
-      <Space h="xl" />
-      <Title order={2}>Settings</Title>
-      {/* <Space h="md" />
-      <Title order={3}>Sharing</Title>
-      <Space h="sm" />
-      <Text>
-        Share the availability of your vacation home by setting the below option
-        to Shared. If you no longer wish to share the availability, simply set
-        it back to Private.
-      </Text>
-      <Space h="sm" />
-      <HomeShareSelect homeId={homeId as string} shared={home.shared} />
-      <Space h="sm" />
-      <CopyButton value={`http://localhost:3000/${homeId}`}>
-        {({ copied, copy }) => (
-          <Button
-            color={copied ? "teal" : "blue"}
-            onClick={copy}
-            variant="light"
-          >
-            {copied ? "Copied!" : "Copy link"}
-          </Button>
-          )}
-        </CopyButton> */}
       <Space h="md" />
       <Title order={3}>Danger zone</Title>
       <Space h="sm" />
-      <DeleteHomeButton homeId={homeId as string} />
+      <HomeDeleteButton homeId={homeId} />
     </main>
-  );
-};
+  </SWRConfig>
+);
 
 Page.getLayout = (children: any) => (
   <DashboardLayout>{children}</DashboardLayout>
@@ -170,10 +59,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   });
 
+  if (home === null) {
+    return { notFound: true };
+  }
+
   return {
     props: {
+      homeId,
       fallback: {
-        [`/api/${homeId}`]: JSON.parse(JSON.stringify(home)),
+        [`/api/homes/${homeId}`]: JSON.parse(JSON.stringify(home)),
       },
     },
   };
