6a4b5dffdbf9d11668f436e115f261a3f8774ca2
Niels Bik <nielsrowinbik@gmail.com>
2022-08-26 21:54:29 +0200

    Add (placeholder) logo component

---
+++ apps/manage/components/Logo.tsx
@@ -0,0 +1,5 @@
+import { HomeIcon } from "@heroicons/react/solid";
+
+export const Logo = () => (
+  <HomeIcon className="h-8 w-8 rounded-full bg-black p-2 text-white dark:bg-white dark:text-zinc-900" />
+);
