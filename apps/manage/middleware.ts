7ca236ae8bfea232adc4af6ff7feaf012e6b8740
Niels Bik <nielsrowinbik@gmail.com>
2022-08-28 13:32:16 +0200

    Remove admin interface (for now)

--- apps/manage/middleware.ts
+++ apps/manage/middleware.ts
@@ -1,11 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/account",
-    "/admin/:path*",
-    "/dashboard/:path*",
-    "/new",
-    "/[homeId]/:path*",
-  ],
+  matcher: ["/account", "/dashboard/:path*", "/new", "/[homeId]/:path*"],
 };
