5e23587f7c6b995664d4754eb02bf4802084fdd5
Niels Bik <nielsrowinbik@gmail.com>
2022-11-15 15:35:19 +0100

    Import classnames correctly

--- apps/web/components/Button.tsx
+++ apps/web/components/Button.tsx
@@ -1,5 +1,5 @@
 import type { ButtonHTMLAttributes } from "react";
-import cn from "classnames";
+import { cn } from "@/lib/utils";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   compact?: boolean;
