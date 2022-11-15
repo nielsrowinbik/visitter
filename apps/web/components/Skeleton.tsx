0731637f1d2cafaf8feaeeadb952ebd7b69a3aab
Niels Bik <nielsrowinbik@gmail.com>
2022-11-15 18:10:29 +0100

    Dark mode for Skeleton component

--- apps/web/components/Skeleton.tsx
+++ apps/web/components/Skeleton.tsx
@@ -7,7 +7,7 @@ export function Skeleton({ className, ...props }: SkeletonProps) {
   return (
     <div
       className={cn(
-        "h-5 w-2/5 animate-pulse rounded-lg bg-zinc-100",
+        "h-5 w-2/5 animate-pulse rounded-lg bg-zinc-100 dark:bg-zinc-700",
         className
       )}
       {...props}
