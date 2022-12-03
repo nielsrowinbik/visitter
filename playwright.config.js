1bc731fa358e834a6d2260d0e12ed69241b73fd5
Niels Bik <nielsrowinbik@gmail.com>
2022-12-03 18:02:43 +0100

    fix: remove webserver config

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,12 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 
 module.exports = {
-  webServer: {
-    command: "npm run start",
-    port: 3000,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "npm run start",
+  //   port: 3000,
+  //   timeout: 120 * 1000,
+  //   reuseExistingServer: !process.env.CI,
+  // },
   use: {
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000",
   },
