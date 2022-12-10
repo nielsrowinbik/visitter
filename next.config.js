e49a1fc272f4d076874a69fa4fb5cafbd74673e8
Niels Bik <nielsrowinbik@gmail.com>
2022-12-10 21:23:31 +0100

    feat: add sentry

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-/** @type {import('next').NextConfig} */
+const { withSentryConfig } = require("@sentry/nextjs");
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["prisma"], // https://github.com/prisma/prisma/issues/16117
   },
+  sentry: {
+    hideSourceMaps: true,
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withSentryConfig(nextConfig, { silent: true });
