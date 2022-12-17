4a160a90233ac9e7d1297110a8e29c6648a2f345
Niels Bik <nielsrowinbik@gmail.com>
2022-12-17 23:38:52 +0100

    test: add e2e tests

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+module.exports = {
+  globalSetup: "./e2e/setup/global-setup.ts",
+  use: {
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000",
+    storageState: "./e2e/setup/storageState.json",
+  },
+};
