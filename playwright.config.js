fe0753f90495026ec9401f7172059d589e747be1
Niels Bik <nielsrowinbik@gmail.com>
2022-12-18 13:36:07 +0100

    test: decrease individual test timeout

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 module.exports = {
   globalSetup: "./e2e/setup/global-setup.ts",
+  timeout: 10 * 1000,
   use: {
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000",
     storageState: "./e2e/setup/storageState.json",
