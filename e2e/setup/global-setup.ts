6b9190f9063dedd4a970f27388d135a279c56ac9
Niels Bik <nielsrowinbik@gmail.com>
2022-12-18 17:43:44 +0100

    refactor: find storage stage through path

--- e2e/setup/global-setup.ts
+++ e2e/setup/global-setup.ts
@@ -1,21 +1,21 @@
 import * as dotenv from "dotenv";
 
-import type { FullConfig } from "@playwright/test";
 import { chromium } from "@playwright/test";
 import { encode } from "next-auth/jwt";
+import path from "path";
 
 dotenv.config();
 
-export default async function globalSetup(config: FullConfig) {
-  const { storageState } = config.projects[0].use;
+export default async function globalSetup() {
+  const storageState = path.resolve(__dirname, "storageState.json");
   const baseURL =
     process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000";
   const domain = new URL(baseURL).hostname;
 
   const secure = domain !== "localhost";
   const sessionToken = await encode({
+    // @ts-ignore
     token: {
-      id: "clbs0af0y0000p2do3lm5kgnr",
       email: "e2e@nielsbik.nl",
     },
     secret: process.env.NEXTAUTH_SECRET!,
