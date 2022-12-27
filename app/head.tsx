89468c93cb109ee5fa66911d1bb7524d66e7ae32
Niels Bik <nielsrowinbik@gmail.com>
2022-12-27 16:07:05 +0100

    feat: add icons

--- app/head.tsx
+++ app/head.tsx
@@ -10,10 +10,30 @@ export default function Head() {
       />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://visitter.app" />
+      {/* TODO: Generate OG image */}
       <meta property="og:image" content="https://visitter.app/og.jpg" />
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://visitter.app" />
+      {/* TODO: Generate OG image */}
       <meta property="twitter:image" content="https://visitter.app/og.jpg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   );
 }
