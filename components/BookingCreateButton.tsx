25bb8199c98b77888b5104be27d7ea1d9c54e4a7
Niels Bik <nielsrowinbik@gmail.com>
2022-12-29 14:14:00 +0100

    feat: use date range input when creating a booking

--- components/BookingCreateButton.tsx
+++ components/BookingCreateButton.tsx
@@ -4,21 +4,67 @@ import * as z from "zod";
 
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import { useController, useForm } from "react-hook-form";
 
 import { Button } from "@/components/Button";
 import type { ButtonProps } from "@/components/Button";
+import type { Control } from "react-hook-form";
+import type { DateChangeCallBack } from "@/components/RangeInput";
 import type { HTMLAttributes } from "react";
 import type { Home } from "@prisma/client";
 import { Icon } from "@/components/Icon";
 import { Input } from "@/components/Input";
-import { bookingCreateSchema } from "@/lib/validations/booking";
+import { RangeInput } from "@/components/RangeInput";
+import { bookingCreateSchema as originalBookingCreateSchema } from "@/lib/validations/booking";
 import superagent from "superagent";
 import { toast } from "@/components/Toast";
-import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+const bookingCreateSchema = originalBookingCreateSchema
+  .omit({
+    startDate: true,
+    endDate: true,
+  })
+  .extend({
+    interval: z.object({
+      start: z.date(),
+      end: z.date(),
+    }),
+  });
+
 type FormData = z.infer<typeof bookingCreateSchema>;
+
+type ControlledRangeInputProps = {
+  control: Control<FormData>;
+};
+
+function ControlledRangeInput({ control }: ControlledRangeInputProps) {
+  const { field } = useController({ control, name: "interval" });
+
+  const handleStartDateChange: DateChangeCallBack = (newStartDate) => {
+    field.onChange({ start: newStartDate, end: field.value.end });
+  };
+
+  const handleEndDateChange: DateChangeCallBack = (newEndDate) => {
+    field.onChange({ start: field.value.start, end: newEndDate });
+  };
+
+  return (
+    <RangeInput
+      onStartDateChange={handleStartDateChange}
+      onEndDateChange={handleEndDateChange}
+    >
+      {({ startDateProps, endDateProps }) => (
+        <div className="flex items-end space-x-3">
+          <Input {...startDateProps} />
+          <Icon.ArrowRight className="mb-4 h-4 w-4 flex-shrink-0" />
+          <Input {...endDateProps} />
+        </div>
+      )}
+    </RangeInput>
+  );
+}
 
 interface BookingCreateButtonProps
   extends HTMLAttributes<HTMLButtonElement>,
@@ -36,19 +82,29 @@ export function BookingCreateButton({
   const openModal = () => setOpen(true);
   const closeModal = () => setOpen(false);
   const {
+    control,
     handleSubmit,
     register,
+    reset,
     formState: { errors },
   } = useForm<FormData>({
+    defaultValues: {
+      name: "",
+      interval: {},
+    },
     resolver: zodResolver(bookingCreateSchema),
   });
   const [isSaving, setIsSaving] = useState<boolean>(false);
 
-  async function onSubmit(data: FormData) {
+  async function onSubmit({ interval, ...data }: FormData) {
     try {
       setIsSaving(true);
 
-      await superagent.post(`/api/homes/${homeId}/bookings`).send(data);
+      await superagent.post(`/api/homes/${homeId}/bookings`).send({
+        startDate: interval.start,
+        endDate: interval.end,
+        ...data,
+      });
 
       router.refresh();
     } catch (error) {
@@ -59,6 +115,7 @@ export function BookingCreateButton({
     } finally {
       setIsSaving(false);
       closeModal();
+      reset();
     }
   }
 
@@ -91,7 +148,7 @@ export function BookingCreateButton({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform divide-zinc-400/20 overflow-hidden rounded-xl bg-white text-left align-middle shadow-xl ring-1 ring-zinc-400/20 transition-all dark:bg-zinc-900">
+                <Dialog.Panel className="w-full max-w-md transform divide-zinc-400/20 rounded-xl bg-white text-left align-middle shadow-xl ring-1 ring-zinc-400/20 transition-all dark:bg-zinc-900">
                   <div className="space-y-4 px-6 pt-6">
                     <div className="flex">
                       <Dialog.Title
@@ -114,20 +171,7 @@ export function BookingCreateButton({
                         label="Name"
                         {...register("name")}
                       />
-                      <Input
-                        errorText={errors.startDate?.message}
-                        required
-                        label="Start date"
-                        type="date"
-                        {...register("startDate")}
-                      />
-                      <Input
-                        errorText={errors.endDate?.message}
-                        required
-                        label="End date"
-                        type="date"
-                        {...register("endDate")}
-                      />
+                      <ControlledRangeInput control={control} />
                     </form>
                   </div>
                   <div className="mt-6 border-t p-6">
