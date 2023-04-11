fb6cd8c725aa485e03ac2882198125e039745c52
Niels Bik <nielsrowinbik@gmail.com>
2023-04-11 13:32:56 +0200

    chore: move to built-in tailwind support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
