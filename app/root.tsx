1be793daa79d24d47e58c317d0ae393d5ab9bf94
Niels Bik <nielsrowinbik@gmail.com>
2023-04-11 13:32:56 +0200

    chore: move to built-in tailwind support

--- app/root.tsx
+++ app/root.tsx
@@ -9,10 +9,11 @@ import {
 
 import type { LinksFunction } from "@remix-run/node";
 import { Toaster } from "react-hot-toast";
+import stylesheet from "~/tailwind.css";
 
 export const links: LinksFunction = () => {
   return [
-    { rel: "stylesheet", href: "/css" },
+    { rel: "stylesheet", href: stylesheet },
     { rel: "manifest", href: "/site.webmanifest" },
   ];
 };
